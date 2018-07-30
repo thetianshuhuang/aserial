f158323e74f760edf62b001833bf99efc340a5c0
thetianshuhuang <thetianshuhuang@gmail.com>
2018-07-29 22:26:56 -0700

    Better examples

--- arduino.py
+++ arduino.py
@@ -1,51 +1,86 @@
 
 """
-0x00 | OPCODE | LENGTH | MSG | CHK
 """
 
-from . import threaded_device
+import time
 import logging
+from . import arduino
+from serial.tools import list_ports
 
 
-class Arduino(threaded_device.ThreadedDevice):
-    """
-    """
+PORT_ID_CACHE = {}
 
-    def __init__(self, port, baudrate=115200, timeout=5):
 
-        super().__init__(port, baudrate=baudrate)
+class ArduinoGetPid(arduino.Arduino):
 
-        if not self.connect(timeout):
-            self.exit_request = True
+    def loop(self):
 
-    def make_chk(self, message):
-        """
-        """
+        self.send(self, 0x00)
 
-        chk = message[0]
-        for char in message:
-            chk ^= char
+        self.pid = self.get()
 
-        return chk
+        # Exit
+        return False
 
-    def send(self, opcode, body):
-        """
-        """
 
-        return self.write(
-            bytes[0, opcode] + body +
-            bytes[self.make_chk(body) ^ opcode])
+def cache_done(devices):
 
-    def get(self):
-        """
-        """
+    for device in devices:
+        if device.done:
+            return True
+    return False
 
-        length = self.read(1)
 
-        msg = self.read(length)
+def refresh_cache(baudrate=115200, timeout=5):
 
-        if msg[-1] != length ^ self.make_chk(msg[:-1]):
-            logging.warning(
-                "Checksum does not match message contents")
+    devices = {}
+    PORT_ID_CACHE = {}
+
+    for port in list_ports.comports():
+        devices[port] = ArduinoGetPid(port, baudrate=baudrate, timeout=timeout)
+
+    while not cache_done:
+        # Sleep 10ms
+        time.sleep(0.01)
+
+    logging.info("Found " + str(len(devices)) + " serial devices:")
+    for name, device in devices.items():
+        devices[name] = device.pid
+        logging.info(name + " | " + device.pid)
+
+    return PORT_ID_CACHE
+
+
+# Refresh cache on module load
+refresh_cache()
+
+
+class ArduinoObject(arduino.Arduino):
+
+    def __init__(self, baudrate=115200, timeout=5):
+
+        if not hasattr(self, "pid"):
+            logging.error(
+                "Arduino object initialized without a PID. No device will " +
+                "be connected.")
+            return False
+
+        for port, device_pid in PORT_ID_CACHE.items():
+
+            if device_pid == self.pid:
+                super().__init__(
+                    port, baudrate=baudrate, timeout=timeout)
+
+                logging.info(
+                    "Device with pid " + str(self.pid) +
+                    " connected at port " + port)
+
+                return True
+
+        return False
+
+    def __str__(self):
+        if hasattr(self, "pid"):
+            return super().__str__ + " with PID" + self.pid
         else:
-            return msg[:-1]
+            return super().__str__ + " without a registered PID"
---
+++ examples.py
@@ -0,0 +1,57 @@
+
+import logging
+from .arduino import ArduinoObject
+
+
+class DriveTrain(ArduinoObject):
+    """
+    Example: drive PWM servo through arduino (8-bit PWM)
+    """
+
+    pid = 0x01
+
+    speed = None
+
+    def set_motor(self, speed):
+        if speed > 256 or speed < 0:
+            logging.error("Attempted to write speed above 256")
+            return False
+
+        self.speed = speed
+        return True
+
+    def loop(self):
+        if self.speed is not None:
+            self.send(0x01, bytes([self.speed]))
+            self.speed = None
+
+        return True
+
+
+drive_train = DriveTrain(baudrate=115200)
+drive_train.set_motor(0)
+
+
+class AnalogSensor(ArduinoObject):
+    """
+    Example: read sensor values
+    """
+
+    pid = 0x02
+    data_available = False
+
+    def loop(self):
+        self.send(0x01, bytes())
+        self.first_sensor = self.get()
+        self.send(0x02, bytes())
+        self.second_sensor = self.get()
+        self.data_available = True
+
+        return True
+
+
+sensor = AnalogSensor(baudrate=115200)
+while not sensor.data_available:
+    pass
+print sensor.first_sensor
+print sensor.second_sensor
